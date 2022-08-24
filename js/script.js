ccae593264628c3971cca10c992a18969ab2827b
Subhransu Sekhar Rath <ssrath38@gmail.com>
2022-08-24 12:42:25 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Nav Menu Open Close
+function  menuToggle(){
+  let navBar = document.getElementById("navBar");
+  navBar.classList.toggle("active");
+  let toggle = document.getElementById("toggle");
+  toggle.classList.toggle("active");
+}
